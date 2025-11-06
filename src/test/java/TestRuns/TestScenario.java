59f329dbf000aa3c621bacc41144a3cd207a7c9b
Scott <scttsmrfng@gmail.com>
2025-11-06 17:11:42 +0800

    Initial Commit

--- src/test/java/TestRuns/TestScenario.java
+++ src/test/java/TestRuns/TestScenario.java
@@ -24,7 +24,7 @@ import static org.hamcrest.Matchers.*;
 
 public class TestScenario extends BasePage {
 	
-	//SCOTTBYRON
+	//SCOTTBYRONESCUETA
 	
 	LandingPage landingpage;
 	TestAPI apitest  = new TestAPI();
