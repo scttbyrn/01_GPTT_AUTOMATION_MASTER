75482e9cc23a13fc9ac9c439feaaf0a84bc2212a
Scott <scttsmrfng@gmail.com>
2025-11-20 14:56:15 +0800

    Test

--- src/test/java/TestComponents/BasePage.java
+++ src/test/java/TestComponents/BasePage.java
@@ -65,8 +65,11 @@ public class BasePage {
 		
 		// BrowserStack options MUST be inside "bstack:options"
 		 Map<String, Object> bstackOptions = new HashMap<>();
-		 bstackOptions.put("userName", "scottbyronescuet_U6GLyK");
-		 bstackOptions.put("accessKey", "Hxd9JCHWySzfyyKxwdSc");
+//		 bstackOptions.put("userName", "scottbyronescuet_U6GLyK");
+//		 bstackOptions.put("accessKey", "Hxd9JCHWySzfyyKxwdSc");
+		 
+		 bstackOptions.put("userName", System.getenv("scottbyronescuet_U6GLyK"));
+		 bstackOptions.put("accessKey", System.getenv("Hxd9JCHWySzfyyKxwdSc"));
 		 bstackOptions.put("deviceName", "Samsung Galaxy S23");
 		 bstackOptions.put("osVersion", "13.0");
 		 bstackOptions.put("projectName", "GPTT App");
